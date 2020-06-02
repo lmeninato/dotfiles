14edc204508ce42eb009b9a4658670c0bcaa8af4
lmeninato <lorenzomeninato@gmail.com>
2020-06-02 19:07:11 -0400

    Initial commit

---
+++ vm_setup.bash
@@ -0,0 +1,10 @@
+sudo apt install git
+sudo apt-get install ntp
+sudo apt install vim
+sudo apt install curl
+
+git config --global user.email "lorenzomeninato@gmail.com"
+git config --global user.name "lmeninato"
+
+xrandr --newmode "1080p" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
+xrandr addmode Virtual1 1080p 
