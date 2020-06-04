6ff6682fa0123a9430e6de793ce1d4db243f509b
lmeninato <lorenzomeninato@gmail.com>
2020-06-04 10:21:10 -0400

    Fix gfx setup

--- vm_setup.bash
+++ vm_setup.bash
@@ -7,4 +7,4 @@ git config --global user.email "lorenzomeninato@gmail.com"
 git config --global user.name "lmeninato"
 
 xrandr --newmode "1080p" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
-xrandr addmode Virtual1 1080p 
+xrandr --addmode Virtual1 1080p 
